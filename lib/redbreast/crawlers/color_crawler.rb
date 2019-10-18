a1cf869157b41b7df430be0196616c627f96ca72
Dundo7 <maroje.marcelic@gmail.com>
2019-10-18 05:40:41 +0200

    Added creating  colors in folders

--- lib/redbreast/crawlers/color_crawler.rb
+++ lib/redbreast/crawlers/color_crawler.rb
@@ -5,7 +5,15 @@ module Redbreast
         def self.color_names_uniq(assets_search_path)
           Dir.glob(assets_search_path).flat_map { |asset_folder|
               Dir.glob("#{asset_folder}/**/*.colorset").map { |color_name|
-                  color_name.split("/")[-1].chomp(".colorset")
+                # arr = color_name.split(".xcassets/")
+                # arr4 = arr.take(1)
+                # puts arr
+                # puts arr4
+              
+                color_name.split(".xcassets/")[-1].chomp(".colorset")
+                # arr[-2] + "/" + arr[-1] 
+
+                # color_name.split("/")[-1].chomp(".colorset")
               }
             }
             .uniq
