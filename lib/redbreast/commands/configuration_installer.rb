dad798ce500a05946a1dc9e16024c424dc970af5
Sven Svetina <sven.svetina@gmail.com>
2025-03-11 16:13:54 +0100

    Add ruby version check and Redbreast install

--- lib/redbreast/commands/configuration_installer.rb
+++ lib/redbreast/commands/configuration_installer.rb
@@ -14,9 +14,6 @@ module Redbreast
         prompt.say('Adding generation script to xcode buid phases...')
         project = fetch_project
         configure_target project.targets.first
-        project.build_configurations.each { |conf| puts conf.name if conf.debug? }
-        #puts "#{project.build_configurations}"
-        #puts "#{project.build_configurations.first.debug?}"
         project.save
         success('Build phase setup!')
       end
@@ -33,7 +30,24 @@ module Redbreast
       def configure_target(target)
         puts target.build_phases.class
         phase = target.new_shell_script_build_phase('Redbreast generate')
-        phase.shell_script = "if DEBUG; then\n PATH=$PATH:~/.rbenv/shims\n redbreast generate\nfi"
+        phase.shell_script = <<~SCRIPT
+          # Ensure correct Ruby version
+          if command -v rbenv &>/dev/null; then
+            eval "$(rbenv init -)"
+            rbenv install -s
+          fi
+
+          # Install Redbreast gem if not present
+          if ! gem list -i redbreast > /dev/null; then
+            gem install redbreast
+          fi
+
+          # Run Redbreast generate only for Debug configurations
+          if [[ "${CONFIGURATION}" == *Debug ]]; then
+            PATH=$PATH:~/.rbenv/shims
+            redbreast generate
+          fi
+        SCRIPT
       end
     end
   end
